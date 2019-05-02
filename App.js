f4c64557934b2805249bc2cb3d58cdc2982fe858
realroy <roiboon.cc@gmail.com>
2019-05-02 15:05:51 +0700

    Init Project.

---
+++ App.js
@@ -0,0 +1,91 @@
+import Expo from "expo";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import { Provider } from "react-redux";
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
+
+import { LOGIN_STATUS_CHANGED } from "./actions/types";
+import { config } from "./consts";
+import firebase from "firebase";
+import reducers from "./reducers";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import NewsScreen from "./screens/NewsScreen";
+import BalanceScreen from "./screens/BalanceScreen";
+import LogoutScreen from "./screens/LogoutScreen";
+//import Test from './screens/Test';
+
+export default class App extends React.Component {
+  //state = { loggedIn: true };
+
+  constructor(props) {
+    super(props);
+    this.store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+  }
+
+  componentWillMount() {
+    console.log(config);
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const AppContainer = createAppContainer(
+      createStackNavigator(
+        {
+          loading_scr: { screen: LoadingScreen },
+          welcome_scr: { screen: WelcomeScreen },
+          register_scr: { screen: RegisterScreen },
+          login_scr: { screen: LoginScreen },
+          main_scr: {
+            screen: createBottomTabNavigator(
+              {
+                news_scr: { screen: NewsScreen },
+                balance_scr: { screen: BalanceScreen },
+                logout_scr: { screen: LogoutScreen }
+              },
+              {
+                tabBarOptions: {
+                  labelStyle: { fontSize: 12 }
+                },
+                swipeEnabled: false,
+                tabBarPosition: "bottom"
+              }
+            )
+          }
+        },
+        {
+          navigationOptions: {
+            tabBarVisible: false
+          },
+          swipeEnabled: false,
+          lazy: true
+        }
+      )
+    );
+
+    return (
+      <Provider store={this.store}>
+        <View style={styles.container}>
+          <AppContainer />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  }
+});
---
+++ screens/NewsScreen.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { View, FlatList } from "react-native";
+import { ListItem } from 'react-native-elements'
+
+
+class NewsScreen extends Component {
+  state = {
+    loading: false,
+    data: [],
+    page: 1,
+    seed: 1,
+    error: null,
+    refreshing: false
+  };
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState(
+      prevState => ({ ...prevState, loading: true }),
+      async () => {
+        try {
+          const res = await fetch(url);
+          const json = await res.json();
+          const data =
+            page === 1 ? json.results : [...this.state.data, ...json.results];
+          this.setState(prevState => ({
+            ...prevState,
+            data,
+            loading: false,
+            refreshing: false,
+            error: json.error
+          }));
+        } catch (error) {
+          this.setState(prevState => ({ ...prevState, error, loading: false }));
+        }
+      }
+    );
+  };
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+  render() {
+    return (
+      <View>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <ListItem
+              roundAvatar
+              leftAvatar={{ source: { uri: item.picture.thumbnail }}}
+              title={`${item.name.first} ${item.name.last}`}
+              containerStyle={{ borderBottomWidth: 0 }}
+            />
+          )}
+          keyExtractor={item => item.email}
+        />
+      </View>
+    );
+  }
+}
+export default NewsScreen;
