2dd9f68e52c73edb6ebe9d31087c38952ab3bd01
realroy <roiboon.cc@gmail.com>
2019-05-03 14:04:23 +0700

    Fix deprecated api.

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { View, FlatList } from "react-native";
-import { ListItem } from 'react-native-elements'
+import { ListItem, Icon } from 'react-native-elements'
 
 
 class NewsScreen extends Component {
